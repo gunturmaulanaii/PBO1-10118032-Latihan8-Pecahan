9315b376c29711a132694e59e0749b05f5a0e373
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-09-25 16:22:51 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan8-Pecahan/src/latihan/pkg8/pecahan/PBO110118032Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg8.pecahan;
+
+/**
+ *
+ * @author
+ * NAMA  : Guntur Maulana Ibrahim
+ * Kelas : PBO1
+ * NIM   : 10118032
+ * Deskripsi Program : Program ini berisi untuk menampilkan tipe-tipe bilangan pecahan
+ */
+public class PBO110118032Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.1234567890F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float\t : " + f);
+        System.out.println("Double\t : " + d);
+    }
+    
+}
